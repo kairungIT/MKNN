385ece62e999289fa3911a3cd348c050b6ae2d5c
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-11-19 14:51:14 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,5 +4,9 @@ import pandas as pd
 st.title("🐷🐷🐷Website Developing using Python🐷🐷")
 st.header("🍖🍖Website Developing using Python🍖🍖")
 
+st.image('./img/kairung.jpg')
 st.subheader("Kairung Hengpraprohm")
 
+dt=pd.read_csv('./data/iris-3.csv')
+st.header("ข้อมูลดอกไม้")
+st.write(dt.head(10))
